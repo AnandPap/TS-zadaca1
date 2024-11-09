73d58ec3c147ef062a6701438d0ce7239f481e62
Anand Pap <anandpap@live.com>
2024-11-09 18:07:46 +0100

    Pobrisan nepotrebni file .gitattributes

--- Zadatak1/obj/Release/net8.0/Zadatak1.AssemblyInfo.cs
+++ Zadatak1/obj/Release/net8.0/Zadatak1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zadatak1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88a785730ea6e67c1fb447aa36117cf8c965601")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zadatak1")]
